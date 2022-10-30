20117afb1f2a7de1e2812ddf02e4eaaf7a12babe
yadjm084 <you@example.com>
2022-10-29 23:11:58 -0400

    db handler, AdminACtivity and UI done only Update a course is not done

--- app/src/main/java/com/example/project/User.java
+++ app/src/main/java/com/example/project/User.java
@@ -4,9 +4,21 @@ public class User {
     protected String username;
     protected String password;
 
+    public User() {
+    }
+
     public User(String username, String password) {
         this.username = username;
         this.password = password;
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     public boolean login(String user, String pass) {
